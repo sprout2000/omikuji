f8d3fcb57abee0cc316dc42fdd52f84316f69ded
sprout2000 <office@nishigami.jp>
2019-06-22 18:03:49 +0900

    devServer is a syntax error now

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,11 @@
 import path from 'path';
 import webpack from 'webpack';
-import devServer from 'webpack-dev-server';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
 import MiniCSSExtractPlugin from 'mini-css-extract-plugin';
 
 const isDev = process.env.NODE_ENV === 'development';
-
-const server: devServer.Configuration = {
-  contentBase: path.resolve(__dirname, 'build'),
-  port: 7000,
-  open: true,
-};
 
 const config: webpack.Configuration = {
   mode: isDev ? 'development' : 'production',
@@ -79,7 +72,6 @@ const config: webpack.Configuration = {
   },
   stats: 'minimal',
   devtool: isDev ? 'source-map' : false,
-  devServer: server,
 };
 
 export default config;
