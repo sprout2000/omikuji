8817ee9d080997a94a28d4381dd3a333b92327c2
sprout2000 <k_konno1983@yahoo.co.jp>
2020-01-01 11:50:30 +0900

    Add mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -27,7 +28,15 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
       },
       {
         test: /\.(bmp|gif|jpe?g|png|svg|ttf|eot|woff?2?)$/,
@@ -50,26 +59,43 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/favicon.ico',
-      chunks: ['app', 'vendor'],
-      filename: 'index.html',
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'assets',
-        to: '.',
-        toType: 'dir',
-      },
-    ]),
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: 'service-worker.js',
-      skipWaiting: true,
-      clientsClaim: true,
-    }),
-  ],
+  plugins: isDev
+    ? [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          favicon: './src/favicon.ico',
+          chunks: ['app', 'vendor'],
+          filename: 'index.html',
+        }),
+        new CopyWebpackPlugin([
+          {
+            from: 'assets',
+            to: '.',
+            toType: 'dir',
+          },
+        ]),
+      ]
+    : [
+        new MiniCssExtractPlugin({}),
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          favicon: './src/favicon.ico',
+          chunks: ['app', 'vendor'],
+          filename: 'index.html',
+        }),
+        new CopyWebpackPlugin([
+          {
+            from: 'assets',
+            to: '.',
+            toType: 'dir',
+          },
+        ]),
+        new WorkboxWebpackPlugin.GenerateSW({
+          swDest: 'service-worker.js',
+          skipWaiting: true,
+          clientsClaim: true,
+        }),
+      ],
   devtool: isDev ? 'inline-source-map' : false,
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
