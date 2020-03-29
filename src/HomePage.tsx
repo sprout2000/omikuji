922a4efedc3d404bf7937f2ac8920c6f10e28a50
sprout2000 <k_konno1983@yahoo.co.jp>
2020-03-29 17:27:37 +0900

    Update .eslintrc & lint

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -23,27 +23,27 @@ const IMAGES = [
 
 const HomePage = (props: Props): JSX.Element => (
   <Page>
-    <div className='container'>
-      <div className='omikuji-container'>
-        <img src={IMAGES[props.imgNum]} alt='logo' className={props.cName} />
+    <div className="container">
+      <div className="omikuji-container">
+        <img src={IMAGES[props.imgNum]} alt="logo" className={props.cName} />
       </div>
-      <div className='button-container'>
+      <div className="button-container">
         <Button onClick={props.omikuji} disabled={props.disable ? true : false}>
-          <Icon icon='fa-tags' style={{ marginRight: 5 }} />
+          <Icon icon="fa-tags" style={{ marginRight: 5 }} />
           おみくじ
         </Button>
       </div>
-      <div className='card-container'>
+      <div className="card-container">
         <Card>
-          <div className='card-header'>
+          <div className="card-header">
             <Icon
-              icon='md-face'
+              icon="md-face"
               size={24}
               style={{ color: '#e91e63', marginRight: 10 }}
             />
             きょうの占い
           </div>
-          <div className='card-content'>
+          <div className="card-content">
             <p>{props.oracle}</p>
           </div>
         </Card>
