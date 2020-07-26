6d556adc394b8d785f3567d21f2a9ea5e5d9c090
sprout2000 <office@nishigami.jp>
2020-07-27 07:43:47 +0900

    fix: Props types

---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Page, List, ListItem, Icon } from 'react-onsenui';
+
+import pjson from '../../package.json';
+
+interface Props {
+  onReload: () => void;
+  onConfirm: () => void;
+}
+
+const SideBar: React.FC<Props> = (props) => (
+  <Page>
+    <List className="drawer">
+      <div className="drawer-header">
+        <p>おみくじ</p>
+        <p>v{pjson.version}</p>
+      </div>
+      <ListItem tappable onClick={props.onReload}>
+        <div className="left">
+          <Icon icon="md-refresh" style={{ color: '#007aff' }} size={24} />
+        </div>
+        <div className="center sidebar-text">アプリを再読込み</div>
+      </ListItem>
+      <ListItem tappable onClick={props.onConfirm}>
+        <div className="left">
+          <Icon icon="md-delete" style={{ color: '#e91e63' }} size={24} />
+        </div>
+        <div className="center sidebar-text">履歴を消去</div>
+      </ListItem>
+      <ListItem tappable>
+        <div className="left">
+          <Icon icon="md-info" style={{ color: '#999' }} size={24} />
+        </div>
+        <div className="center sidebar-text">
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/sprout2000/omikuji">
+            ソースコードURL
+          </a>
+        </div>
+      </ListItem>
+    </List>
+  </Page>
+);
+
+export default SideBar;
