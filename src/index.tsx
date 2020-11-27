10aa7d6f0571c1c8fd120d13e38b3b21f0468caa
sprout2000 <office@nishigami.jp>
2020-11-27 13:57:28 +0900

    fix: PWA window sizes on Win10

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import ons from 'onsenui';
 
 import App from './Components/App';
+import { forceScreenSize } from './forceScreenSize';
 
 import 'onsenui/css/onsenui.min.css';
 import 'onsenui/css/onsen-css-components.min.css';
 import './styles.css';
 
-import { forceScreeSize } from './forceScreenSize';
-forceScreeSize(400, 600);
-
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  window.addEventListener('load', (): void => {
+  window.addEventListener('resize', () => {
+    forceScreenSize(400, 600);
+  });
+
+  window.addEventListener('load', () => {
+    forceScreenSize(400, 600);
     navigator.serviceWorker.register('./service-worker.js');
   });
 }
